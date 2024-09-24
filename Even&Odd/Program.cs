bf14fe1caa0721a0a139bcb38f8dd62edde80de3
davit <davit@GOSH>
2024-09-24 13:18:41 +0400

    Add project files.

---
+++ Even&Odd/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Sheiyvanet mteli ricxvi:");
+
+
+int Weli = int.Parse(Console.ReadLine());
+
+if (Weli % 2 == 0)
+{
+    Console.WriteLine($"ricxvi luwia");
+}
+else if (Weli % 2 != 0)
+{
+    Console.WriteLine($"ricxvi kentia");
+}
+else
+{
+    Console.WriteLine($"araswori simbolo");
+}
